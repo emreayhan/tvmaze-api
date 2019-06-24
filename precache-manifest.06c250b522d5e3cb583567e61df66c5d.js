6a8d0a6a07476eac79dd60e41886f6d6aab0b5a3
emre ayhan <emre.ayhan@adesso.com.tr>
2019-06-24 14:35:03 +0300

    Updates

--- precache-manifest.06c250b522d5e3cb583567e61df66c5d.js
+++ precache-manifest.06c250b522d5e3cb583567e61df66c5d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e137c164283aaac97c94860f34f1121",
+    "revision": "b406122ea2489d9ad529707aadf3704f",
     "url": "/tvmaze-api/index.html"
   },
   {
-    "revision": "06d15559be04873ecb01",
+    "revision": "c5835180e8bbaaf1c4d2",
     "url": "/tvmaze-api/static/css/main.3b6800c1.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tvmaze-api/static/js/2.d5a9611a.chunk.js"
   },
   {
-    "revision": "06d15559be04873ecb01",
-    "url": "/tvmaze-api/static/js/main.bd90783b.chunk.js"
+    "revision": "c5835180e8bbaaf1c4d2",
+    "url": "/tvmaze-api/static/js/main.962045aa.chunk.js"
   },
   {
     "revision": "5f599398a66ae77fe1a9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/tvmaze-api/precache-manifest.cfd707491048be1f6f107030ff7a1b93.js"
+  "/tvmaze-api/precache-manifest.06c250b522d5e3cb583567e61df66c5d.js"
 );
 
 self.addEventListener('message', (event) => {
