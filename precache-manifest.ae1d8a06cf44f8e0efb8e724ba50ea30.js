f1a97fc4e5ae70b9c0e65aa8a0917bc323a443dc
emre ayhan <emre.ayhan@adesso.com.tr>
2019-06-24 12:33:48 +0300

    Updates

---
+++ precache-manifest.ae1d8a06cf44f8e0efb8e724ba50ea30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7c27c98f4b6105d10b811afb7abd96",
+    "url": "/tvmaze-api/index.html"
+  },
+  {
+    "revision": "c1e62784a6e6c3d4b87d",
+    "url": "/tvmaze-api/static/css/main.3b6800c1.chunk.css"
+  },
+  {
+    "revision": "099a4183238f538cbbd7",
+    "url": "/tvmaze-api/static/js/2.d5a9611a.chunk.js"
+  },
+  {
+    "revision": "c1e62784a6e6c3d4b87d",
+    "url": "/tvmaze-api/static/js/main.4bcd5081.chunk.js"
+  },
+  {
+    "revision": "5f599398a66ae77fe1a9",
+    "url": "/tvmaze-api/static/js/runtime~main.93d8631f.js"
+  }
+]);
